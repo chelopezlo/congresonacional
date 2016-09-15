18649ecb76af2bf38d0ecfafef5973c0d4470ccb
Eliecer Lopez <eliecer.lopez.lopez@gmail.com>
2016-09-14 22:25:49 -0300

    Consulta al servidor la información a mostrar en la aplicación

--- www/js/app.js
+++ www/js/app.js
@@ -12,14 +12,24 @@ $.widget( "prodev.card", {
         image: "icons/coffee.png",
         title: "Comida",
         text: "fecha",
-        type : "",
+        type : "",        
+        status: "BLOCKED",
         id : "",
         html: ""
     },
  
     _create: function() {
         var box = '';
-        box += '    <button class="ui-btn ui-btn-inline waves-effect waves-button">';
+        var status = 'disabled';
+        if(this.options.status == 'BLOCKED')
+        {
+            status = 'disabled';
+        }
+        else if(this.options.status == 'OPEN')
+        {
+            status = '';
+        }
+        box += '    <button class="ui-btn ui-btn-inline waves-effect waves-button" ' + status + '>';
         box += '        <div class="nd2-card card-media-right card-media-small" id="' + this.options.id + '">';
         box += '            <div class="card-media"><img src="./img/' + this.options.image + '"></div>';
         box += '            <div class="card-title">';
@@ -28,6 +38,34 @@ $.widget( "prodev.card", {
         box += '            </div>';
         box += '        </div>';
         box += '    </button>';
+
+        //this.element.append(box);
+        this.options.html = box;
+    }
+ 
+});
+
+$.widget( "prodev.avatar", {
+ 
+    // Default options.
+    options: {
+        image: "./img/logo.png",
+        title: "Nombre no disponible",
+        text: "Información no disponible",
+        type : "",
+        id : "",
+        html: ""
+    },
+ 
+    _create: function() {
+        var box = '';
+        box += '    <div class="nd2-card card-media-right card-media-small wow fadeInDown" data-id="' + this.options.id  + '">';
+        box += '        <div class="card-media"><img src="' + this.options.image + '"></div>';
+        box += '        <div class="card-title">';
+        box += '            <h3 class="card-primary-title">' + this.options.title + '</h3>';
+        box += '            <h6 class="card-subtitle">' + this.options.text + '</h6>';
+        box += '        </div>';
+        box += '    </div>';
 
         //this.element.append(box);
         this.options.html = box;
